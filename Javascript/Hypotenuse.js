872684ee32ec08bb9ba276018398774dd9e7c1a1
Jatin More <54055686+jatinmore@users.noreply.github.com>
2021-08-23 01:44:27 +0530

    Hypotenuse quiz done

---
+++ Javascript/Hypotenuse.js
@@ -0,0 +1,16 @@
+var sideInput=document.querySelectorAll('.side-inputs');
+var btnHypotenuse=document.querySelector('#btn-hypotenuse');
+var output=document.querySelector('#outputAns');
+
+btnHypotenuse.addEventListener('click',calculateHypotenuse);
+
+function calculateHypotenuse()
+{
+    const a=Number(sideInput[0].value);
+    const b=Number(sideInput[1].value);
+    const sumOfSides=a*a + b*b;
+    const hypotenuseVal=Math.sqrt(sumOfSides)
+
+    output.innerText= 'The length of the hypotenuse is = '+ hypotenuseVal;
+    console.log(sumOfSides)
+}
